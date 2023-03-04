384fc5d1c97e5524640b7d17b28bd926dadb720f
LuisGuilhermeMissura <62038111+LuisGuilhermeMissura@users.noreply.github.com>
2023-03-03 22:03:15 -0300

    Add files via upload

---
+++ BalancoTrimestral/src/BalancoTrimestral.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * @author guibm
+ */
+public class BalancoTrimestral {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        double gastosJaneiro = 15000;
+        double gastosFevereiro = 23000;
+        double gastosMarco = 17000;
+        double balancoTrimestral;
+        
+        
+        
+        
+        balancoTrimestral = (gastosJaneiro + gastosFevereiro + gastosMarco)/3;
+        
+        System.out.println("balancoTrimestral");
+        
+        
+    }
+}
